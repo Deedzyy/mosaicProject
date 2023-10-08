5b34b0fb1be10d5ee584dfee2099ad6040fd9285
Deedzyy <william.morhedec@viacesi.fr>
2023-10-08 15:58:52 +0200

    Header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HomePageComponent } from './home-page/home-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomePageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      { path: 'homepage', component: HomePageComponent },
+      { path: '',   redirectTo: 'homepage', pathMatch: 'full' }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
