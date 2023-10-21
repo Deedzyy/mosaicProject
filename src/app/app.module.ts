58795ec1a2133248124f9a715d0ac92fa0981290
Deedzyy <william.morhedec@viacesi.fr>
2023-10-21 13:49:08 +0200

    register component creation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { LoginComponentComponent } from './login-component/login-component.component';
 import { HomeComponent } from './home/home.component';
+import { RegisterComponentComponent } from './register-component/register-component.component';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { HomeComponent } from './home/home.component';
     HeaderComponent,
     LoginComponentComponent,
     HomeComponent,
+    RegisterComponentComponent,
    ],
   imports: [
     BrowserModule,
---
+++ src/app/register-component/register-component.component.ts
@@ -0,0 +1,11 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-register-component',
+  templateUrl: './register-component.component.html',
+  styleUrls: ['./register-component.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class RegisterComponentComponent {
+
+}
